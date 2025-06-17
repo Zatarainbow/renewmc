6ea2283bc652dedc15b19731072a8e2e41c4caa5
Zatarainbow <144702196+Zatarainbow@users.noreply.github.com>
2025-06-17 21:51:16 +0700

    Update renew.py

--- renew.py
+++ renew.py
@@ -11,7 +11,7 @@ sryzenhd1={"Cookie":"connect.sid=s%3Af-9bZ9RxC2760zbmTraekOINSQqp2X1u.I0eImANx8E
 sryzenhd2={"Cookie":"connect.sid=s%3ApsoFob1rBrAMB77kCmGe6c_MEL7Mq6fA.dYNcruTX2nkWK08WIl0FBXkfaRi3axCO8pfweFuQk5g"}
 
 lemem_headers = {
-      "Cookie": "cf_clearance=3ZE8Z3znpczPP0kjn6pNZOnxPp8xvMrNnwUbiKXkpLA-1750152115-1.2.1.1-Jlg.uBBHBs5R.MKRrfp.RmUN9JGD82sib0L4Z0RpMBogapury.VV68aTSKQ8uEt.Z35kfSY6U.c9MacYo13b29Mww2bUe7f6ZiCK.BmEHB1Vvegj65f__c_MX9ZU3s6S3wj5zMXOCMl03wOq5zF.2iVnM3JV_rYxnVJboVefambluAG9DucPYpO.n7QmNSzzDzU2rDSv0tcxkirDQigr864MiWEiTmj3lZ6KDaq.HrYGLrILXXzeggE640.CiXgDa3htZysEg0OHZ4YFAygWiB8oKcKW1F0JUEv_AhhB6yWXYLXNs7QsOpn2LR35oXTq40DVeyYKIpl89CAlXI4tQ6dN7gUrtcTXxSk4cI3DKXMpYGCuu9Iyjo83QZ3ARt2y; sid=s.X05wUcfxaY33YSpNoar82ltJFxKXwZlbd8S8egCXtx56eg17D0aBD4HyZ9ig",
+      "Cookie": "cf_clearance=vgmkuUwQ019oKGMXruGeWDGmlq4l_X_vv9pEnS76_AA-1750171779-1.2.1.1-cNZw.euTuXsubr25YkkEGPbez8KQtsE79OX.9bp1OSNrXbw6FRhn.tBd5ju9yTfDL.Ba6iLbfAwgClqnLAR2ajK9HklWmDMjg7Jkk4VRHSwX2.J_xTZ4sAU11WQJv4_tRfZgXkYYek5sGDA66wXIj9AcWR_.jhKQYrWy33Jo9Pw9WKxoKYqWPsOoMxiMmeJ.EV8p9o5gZa2lYRGCKJPtkCmVtwNLP.ggi18FUa0TqzHEuydT0afedp2MsIS3jh5BMfjY.mH3dmlz4VOFaszycktrNf1pCowNeIhWz6tpMTYaT72HF4w_s9KAEQe2uShUnAEnRmJYX6K8dlgInVMNyasWD_EgKx.el82Eherv9xABa80I2RBceIXmGUPgsAt5; sid=s.X05wUcfxaY33YSpNoar82ltJFxKXwZlbd8S8egCXtx56eg17D0aBD4HyZ9ig",
       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0",
 }
 
